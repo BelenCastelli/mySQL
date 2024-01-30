187737c61ea64b3938964759b13de79dbf542f0c
Belen Castelli <mariabelencastelli29@gmail.com>
2024-01-30 17:32:38 +0100

    reto2

--- course.js
+++ course.js
@@ -16,7 +16,13 @@ async function main (){
         // createTable(connection)
         // insert(connection)
         // alterTable(connection);
-        deleteTable(connection);
+        // deleteTable(connection);
+        // update(connection);
+        // select(connection);
+        // deleteMark(connection);
+        // updateMark(connection);
+        // count(connection);
+        // join(connection);
 
 
 
@@ -38,28 +44,6 @@ async function createTable(connection){
         let [result] = await connection.query(sql);
         console.log("Tabla creada");
         console.log(result);
-}
-
-async function insert(connection){
-    // let sql = "INSERT INTO `subjects` (title) " +
-    //             "VALUES('Psicología Forense y Criminología'), ('Motivación y Emoción'), ('Psicología del Lenguaje'), ('Terapias Psicológicas'),('Psicología de la Salud'), ('Adquisición del Conocimiento'), ('Psicología de los Grupos'),('Psicología de la Educación'), ('Etología'), ('Trastornos del neurodesarrollo') "
-                                 
-    // let [result] = await connection.query(sql);
-    // let groups = "INSERT INTO `groups` (name) " +
-    //                 "VALUES('4B'), ('4C'), ('5A'), ('5B'), ('5C'), ('6A'), ('6B'), ('6C'), ('7A'), ('7B'), ('7C')"
-
-    // let [result] = await connection.query(groups);
-    // result.insertId = 11
-
-    // let groups = "INSERT INTO `students` (first_name, last_name, group_id) " +
-    // "VALUES('Juan', 'Ballesta', 5), ('Cristina', 'Cuevas', 6), ('Angela', 'Rivas', 6)"
-
-    let [result] = await connection.query(groups);
-    result.insertId = 8
-
-    console.log("dato insertado");
-    console.log(result);
-
 }
 
 async function alterTable(connection){
@@ -86,6 +70,77 @@ async function deleteTable(connection){
     console.log(result);
 }
 
+async function insert(connection){
+    // let sql = "INSERT INTO `subjects` (title) " +
+    //             "VALUES('Psicología Forense y Criminología'), ('Motivación y Emoción'), ('Psicología del Lenguaje'), ('Terapias Psicológicas'),('Psicología de la Salud'), ('Adquisición del Conocimiento'), ('Psicología de los Grupos'),('Psicología de la Educación'), ('Etología'), ('Trastornos del neurodesarrollo') "
+                                 
+    // let [result] = await connection.query(sql);
+    // let groups = "INSERT INTO `groups` (name) " +
+    //                 "VALUES('4B'), ('4C'), ('5A'), ('5B'), ('5C'), ('6A'), ('6B'), ('6C'), ('7A'), ('7B'), ('7C')"
+
+    // let [result] = await connection.query(groups);
+    // result.insertId = 11
+
+    let groups = "INSERT INTO `students` (first_name, last_name, group_id) " +
+    "VALUES('Juan', 'Ballesta', 5), ('Cristina', 'Cuevas', 6), ('Angela', 'Rivas', 6)"
+
+    let [result] = await connection.query(groups);
+    result.insertId = 8
+
+    console.log("dato insertado");
+    console.log(result);
+
+}
+
+async function update(connection){
+
+    let marks = "UPDATE `marks` SET mark = 0"
+    let [result] = await connection.query(marks)
+    console.log('Notas modificadas');
+    console.log(result);
+    
+}
+
+async function select(connection){
+    let students = "SELECT first_name, last_name FROM students"
+    let [result] = await connection.query(students)
+    console.log('Datos obtenidos');
+    console.log(result);
+
+    let teacher = "SELECT * FROM teachers"
+    let [result2] = await connection.query(teacher)
+    console.log(result2);
+}
+
+async function deleteMark(connection){
+    let marks = "DELETE FROM marks WHERE date < '2014-01-30'"
+    let [result] = await connection.query(marks)
+    console.log(result);
+}
+
+async function updateMark(connection){
+    let marks = "UPDATE marks SET mark=5 WHERE mark < 5"
+    let [result] = await connection.query(marks)
+    console.log(result);
+}
+
+async function count(connection){
+    let totalElements = "SELECT COUNT (*) FROM subjects"
+    let [result] = await connection.query(totalElements)
+    console.log(result);
+}
+
+async function join(connection){
+    let join = `
+    SELECT students.first_name, subjects.title
+    FROM marks
+    JOIN students ON marks.student_id = students.students_id
+    JOIN subjects ON marks.subject_id = subjects.subjects_id
+  `
+    let [result] = await connection.query(join)
+    console.log(result);
+
+}
 
 
 main()
